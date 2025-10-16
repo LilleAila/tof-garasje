de36eff9a8ef24825675eeacdf43bf8268809b97
IldenH <IldenH.1@proton.me>
2025-10-16 10:17:03 +0200

    servo: looping motor

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,7 +1,22 @@
-void setup() {
+// https://docs.arduino.cc/learn/electronics/servo-motors/
+#include <Servo.h>
 
+Servo myservo;
+int pos;
+
+
+void setup() {
+  myservo.attach(9);
 }
 
-void loop() {
 
+void loop() {
+  for (pos = 0; pos <= 180; pos++) {
+    myservo.write(pos);
+    delay(15);
+  }
+  for (pos = 180; pos >= 0; pos--) {
+    myservo.write(pos);
+    delay(15);
+  }
 }
