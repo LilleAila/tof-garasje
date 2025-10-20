86703300fecdd488f0d2b95826cdc8f721a3430b
IldenH <IldenH.1@proton.me>
2025-10-20 15:02:02 +0200

    servo: 90 deg turns

--- servo/servo.ino
+++ servo/servo.ino
@@ -11,12 +11,16 @@ void setup() {
 
 
 void loop() {
-  for (pos = 0; pos <= 180; pos++) {
-    myservo.write(pos);
-    delay(15);
-  }
-  for (pos = 180; pos >= 0; pos--) {
-    myservo.write(pos);
-    delay(15);
-  }
+  myservo.write(0);
+  delay(1000);
+  myservo.write(90);
+  delay(1000);
+  // for (pos = 0; pos <= 180; pos++) {
+  //   myservo.write(pos);
+  //   delay(15);
+  // }
+  // for (pos = 180; pos >= 0; pos--) {
+  //   myservo.write(pos);
+  //   delay(15);
+  // }
 }
