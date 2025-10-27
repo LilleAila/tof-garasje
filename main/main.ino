f39d6a7ffc2ebe2febde2b613ae3b4a93d2144df
IldenH <IldenH.1@proton.me>
2025-10-27 13:58:55 +0100

    Revert "main: multiple files"
    This reverts commit 20f4692e5f6dae22f7681064d9389213e7ab4b36.

--- main/main.ino
+++ main/main.ino
@@ -2,25 +2,58 @@
   * https://projecthub.arduino.cc/Isaac100/getting-started-with-the-hc-sr04-ultrasonic-sensor-7cabe1
   * https://docs.arduino.cc/learn/electronics/servo-motors/
   * */
-#include "Door.h"
-#include "Ultrasonic.h"
+#include <Servo.h>
 
+Servo myservo;
 const int servoPin = 11;
+int pos = 90;
+bool doorOpen;
+unsigned long doorOpenedAt = 0;
+const unsigned long openDuration = 3000;
+
 const int trigPin = 9;
 const int echoPin = 10;
 
-Door door(servoPin);
-Ultrasonic sensor(trigPin, echoPin);
+float duration, distance;
+
+
 
 void setup() {
-  door.begin();
-  sensor.begin();
+  myservo.attach(servoPin);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  Serial.begin(9600);
 }
 
-void loop() {
-  float distance = sensor.getDistance();
-  unsigned long now = millis();
 
-  door.update(distance, now);
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2; // Speed of sound in micrometers per second
+	Serial.println(distance);
+
+
+  unsigned long now = millis();
+  if (distance < 50) {
+    doorOpenedAt = now;
+
+    if (!doorOpen) {
+      myservo.write(90);
+      doorOpen = true;
+    }
+  }
+
+  if (doorOpen && millis() - doorOpenedAt >= openDuration) {
+    myservo.write(0);
+    doorOpen = false;
+  }
+
   delay(100);
 }
